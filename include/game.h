597243901a971d0a6269b1f0c93316d1f8791b65
kvasa <kvasan2104@gmail.com>
2023-06-25 18:12:21 +0300

    changes

---
+++ include/game.h
@@ -0,0 +1,20 @@
+#ifndef KVASOV_ANDREY_IU4_23B_RK1_GAME_H
+#define KVASOV_ANDREY_IU4_23B_RK1_GAME_H
+
+#define TEXT_1 "Some text to print on screen large enough to test all the features until my fantasy comes up with " \
+"something more original and suitable for a simple level of the game. OKAY that text wasn't large enough. So if you " \
+"thought that would stop me... you underestimated me! I will continue typing nonsense until my needs are fulfilled!"
+
+typedef struct Statistics {
+    int correct_symb_num;
+    int incorrect_symb_num;
+    int total_symb_num;
+    float average_symb_speed;
+    float average_word_speed;
+} Statistics_t;
+
+void print_statistics(Statistics_t statistics);
+
+void check_args(char *difficulty, int time_limit, int error_limit);
+
+#endif
--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
+#include "include/game.h"
 
-int main() {
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        puts("Wrong number of args!");
+        exit(EXIT_FAILURE);
+    }
+    char *difficulty = argv[1];
+    int time_limit = strtol(argv[2], NULL, 10);
+    int error_limit = strtol(argv[3], NULL, 10);
+
+
+
     initscr();
-    printw("Hello world");
+    printw("%s", TEXT_1);
     refresh();
     getch();
     endwin();
-    return 0;
+    return ;
 }
